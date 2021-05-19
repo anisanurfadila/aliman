2629b21f936502b78809f5189eabe42d43647d52
anisanurfadila <anisanfd925@gmail.com>
2021-05-19 23:38:45 +0700

    create new files

---
+++ app/src/main/java/com/example/aliman/ActivityDzikirCounter.java
@@ -0,0 +1,14 @@
+package com.example.aliman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityDzikirCounter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dzikir_counter);
+    }
+}
---
+++ app/src/main/java/com/example/aliman/ClassDzikir.java
@@ -0,0 +1,4 @@
+package com.example.aliman;
+
+public class ClassDzikir {
+}
